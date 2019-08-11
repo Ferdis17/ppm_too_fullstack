bc0124cfbe8bb5290a15f751cf06e13fe2d02a8e
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-08-11 15:57:23 -0400

    first commit

---
+++ ppmtool_full_stack/src/main/java/ferdis/personal/project/tool/ppm_tool/PpmToolApplication.java
@@ -0,0 +1,13 @@
+package ferdis.personal.project.tool.ppm_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolApplication.class, args);
+    }
+
+}
