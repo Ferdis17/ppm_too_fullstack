901ee6cf0571f625f769a6aa273e98c04409fe5e
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-08-12 20:17:20 -0400

    react project

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from "./components/Project/AddProject"
+
+class App extends Component {
+    render() {
+        return (
+            < Router >
+            < div
+        className = "App" >
+
+            < Header / >
+            < Route
+        exact
+        path = "/dashboard"
+        component = {Dashboard}
+        />
+        < Route
+        exact
+        path = "/addProject"
+        component = {AddProject}
+        />
+        < /div>
+        < /Router>
+
+    )
+        ;
+    }
+
+}
+
+export default App;
---
+++ ppmtool_full_stack/src/main/java/ferdis/personal/project/tool/ppm_tool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,19 @@
+package ferdis.personal.project.tool.ppm_tool.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+}
